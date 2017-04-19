c9af3a622a0863c6e9a0b2d7ce7232cbdd03766e
Lawrence Lee <lfqlee314@gmail.com>
2017-04-18 21:15:43 -0400

    add project table, fix other spacing shit

--- js/scroll.js
+++ js/scroll.js
@@ -1,6 +1,7 @@
 $(document).ready(function(){
     $(window).scroll(function(){
-        $(".navbar").css("opacity", 1 - $(window).scrollTop() / 250);
-        $(".panel").css("opacity", $(window).scrollTop() / 250);
+        $(".navbar").css("opacity", 1 - $(window).scrollTop() / 100);
+        $(".panel").css("opacity", $(window).scrollTop() / 100);
+        $(".table").css("opacity", $(window).scrollTop() /100);
     });
 });
