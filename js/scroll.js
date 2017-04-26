b67ff82843ded002ea06d1ebfc1e6e1bec498b0c
Lawrence Lee <lfqlee314@gmail.com>
2017-04-26 01:09:49 -0400

    add background parallax

--- js/scroll.js
+++ js/scroll.js
@@ -3,7 +3,7 @@ $(document).ready(function(){
         $(".navbar").css("opacity", 1 - $(window).scrollTop() / 200);
         // $(".panel").css("opacity", $(window).scrollTop() / 100);
         // $(".table").css("opacity", $(window).scrollTop() /100);
-        $(".down-container").css("opacity", 1 - $(window).scrollTop() / 100);
+        $(".down-container, .page-content").css("opacity", 1 - $(window).scrollTop() / 100);
     });
 });
 
