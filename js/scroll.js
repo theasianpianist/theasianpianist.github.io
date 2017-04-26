ea5bc4fa21f4ab9b24d187492c5cee0b095216fa
Lawrence Lee <lfqlee314@gmail.com>
2017-04-26 04:34:59 -0400

    fix shit

--- js/scroll.js
+++ js/scroll.js
@@ -6,22 +6,23 @@ $(document).ready(function(){
         $(".down-container, .page-content").css("opacity", 1 - $(window).scrollTop() / 200);
 
     });
-});
-
-$( ".expandable" ).click(function() {
+    $( ".expandable" ).click(function() {
     $(this).toggleClass("expand");
+	});
+
+	$(".down").click(function() {
+	    $('html,body').animate({
+	        scrollTop: $(".main").offset().top},
+	        'slow');
+	});
+
+	
+
 });
 
-$(".down").click(function() {
-    $('html,body').animate({
-        scrollTop: $(".main").offset().top},
-        'slow');
+$(".no-propogate").click( function(f) {
 });
 
-$(".clickable-row").click( function() {
+$(".clickable-row").click( function(e) {
     window.location = $(this).find('a').attr('href');
-});
-
-$(".no-propogate").click( function(e) {
-	e.stopPropogation();
 });
