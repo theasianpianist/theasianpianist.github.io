7d682d7e21446fbbb0459b84587bd65b3b82196d
Lawrence Lee <lfqlee314@gmail.com>
2017-04-25 23:41:32 -0400

    everything works

--- js/scroll.js
+++ js/scroll.js
@@ -1,8 +1,8 @@
 $(document).ready(function(){
     $(window).scroll(function(){
         $(".navbar").css("opacity", 1 - $(window).scrollTop() / 200);
-        $(".panel").css("opacity", $(window).scrollTop() / 100);
-        $(".table").css("opacity", $(window).scrollTop() /100);
+        // $(".panel").css("opacity", $(window).scrollTop() / 100);
+        // $(".table").css("opacity", $(window).scrollTop() /100);
         $(".down-container").css("opacity", 1 - $(window).scrollTop() / 100);
     });
 });
