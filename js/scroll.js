00652a199d914890936cb270547baf97df82d829
Lawrence Lee <lfqlee314@gmail.com>
2017-04-26 03:04:06 -0400

    make rows clickable

--- js/scroll.js
+++ js/scroll.js
@@ -1,9 +1,10 @@
 $(document).ready(function(){
     $(window).scroll(function(){
         $(".navbar").css("opacity", 1 - $(window).scrollTop() / 200);
-        $(".panel").css("opacity", $(window).scrollTop() / 100);
-        $(".table").css("opacity", $(window).scrollTop() /100);
+        $(".panel").css("opacity", $(window).scrollTop() / 200);
+        $(".table").css("opacity", $(window).scrollTop() / 200);
         $(".down-container, .page-content").css("opacity", 1 - $(window).scrollTop() / 200);
+
     });
 });
 
@@ -15,4 +16,12 @@ $(".down").click(function() {
     $('html,body').animate({
         scrollTop: $(".main").offset().top},
         'slow');
+});
+
+$(".clickable-row").click( function() {
+    window.location = $(this).find('a').attr('href');
+});
+
+$(".no-propogate").click( function(e) {
+	e.stopPropogation();
 });
